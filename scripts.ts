7d33f6bf478ac6d84897652aad7d4be13897d799
JIMMY JUNG <idjjm92@gmail.com>
2023-09-20 23:15:20 +0100

    script added

---
+++ scripts.ts
@@ -0,0 +1,16 @@
+const scripts = [
+  {
+    title: "memories I almost died in the army",
+    words: [
+      "squad",
+      "gear",
+      "tank lorry",
+      "basecamp",
+      "supply",
+      "ominous",
+      "구난차",
+    ],
+    description:
+      "When I was in the army, I used to drive a tank lorry which supplies petrol to tanks, one day, I was on the way back to the basecamp, I could feel some ominous feeling through the gear grip and then right after, I couldn't control the gear bar. but the tank was filled up with petrol even 구난차 couldn't come to drag me all the way to the basecamp. because I was too heavy. ",
+  },
+];
